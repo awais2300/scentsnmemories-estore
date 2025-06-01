d06b6d0ae74535314229f5b96efd2645ce262f84
Awais Ahmad <awais_rafaqat2300@yahoo.com>
2025-06-01 22:11:47 +0500

    few changes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
-const expores = require("express");
+const express = require("express");
 const mongoose = require("mongoose");
-const app = expores();
+const app = express();
 const port = process.env.PORT || 3000;
 var mongoConnection = process.env.mongoConnection || "mongodb://scentsnmemories-azure:wFD9ItSQeuTmlnLBkUBtNLorleOl7JMIIDfiHtuVrgtMBZlxZd5va4IvUznCuKzZXhaeVje9JYKUACDbcN3vbA==@scentsnmemories-azure.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@scentsnmemories-azure@";
 //"mongodb://localhost:27017/e-comm-store-db";
 // || "mongodb://scentsnmemories-azure:IRhI8N0P012RwgpEFSGc0bTSK5UwksYCq6RVNfahrRkGvYTA6Hl5yDha6SrjzwHmeYBG1J1f1hCtACDb5snFBw==@scentsnmemories-azure.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@scentsnmemories-azure@";
 const cors = require("cors");
+app.use(cors({
+  
+  origin: 'https://gray-forest-06c81221e.6.azurestaticapps.net'
+  //origin: 'http://localhost:4200'
+}));
+app.options('*', cors());
+app.use(express.json());
 const categoryRoutes = require("./routes/category");
 const brandRoutes = require("./routes/brand");
 const orderRoutes = require("./routes/order");
@@ -14,13 +21,7 @@ const customerRoutes = require("./routes/customer");
 const authRoutes = require("./routes/auth");
 const { verifyToken, isAdmin } = require("./middleware/auth-middleware");
 const seedData = require("./handlers/data-seed");
-app.use(cors({
-  
-  origin: 'https://gray-forest-06c81221e.6.azurestaticapps.net'
-  //origin: 'http://localhost:4200'
-}));
-app.options('*', cors());
-app.use(expores.json());
+
 app.use((req, res, next) => {
   req.user = { id: "644c1f5f0f3e2a456789abcd" };  // Replace with any fixed user ID for testing
   next();
