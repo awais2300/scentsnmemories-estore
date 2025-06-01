c752b66cbcf7d8cde195ea2c013fccd9a8b8912e
Awais Ahmad <awais_rafaqat2300@yahoo.com>
2025-06-01 16:30:55 +0500

    Update

--- webapp/src/environments/environment.development.ts
+++ webapp/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://scentsnmemories-azure-ccbmfqbvdqchajd0.canadacentral-01.azurewebsites.net/',
+  apiUrl: 'https://scentsnmemories-azure-ccbmfqbvdqchajd0.canadacentral-01.azurewebsites.net',
   //apiUrl:"http://localhost:3000"
 };
